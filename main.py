cc4d0628f11f98bb3cba699578e4478a42929964
lgcanto <lucascanto@poli.ufrj.br>
2017-10-30 00:01:33 -0200

    More testing

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
 import webapp2
 
 form = """
-<form action="/testform">
-<input name="q">
-<input type="submit">
+
+<div>Please, insert your birth date:</div>
+<form method="post">
+    <label>
+        Month
+        <input type="text" name="month" > 
+    </label>
+    <br>
+    <label>
+        Day 
+        <input type="text" name="day" >
+    </label>
+    <br>
+    <label> 
+        Year
+        <input type="text" name="year" >
+    </label>
+    <br>
+    <div style="color: red;">%(message)s</div>
+<input type="submit" value="Submit">
 </form>
+
 """
+# form = 'forms.html'
 
 class MainPage(webapp2.RequestHandler):
+    def write_form(self, message = ""):
+        self.response.out.write(form % {"message": message})
+    
     def get(self):
+        self.write_form()
+    
+    def post(self):
         #self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(form)
-
-class TestHandler(webapp2.RequestHandler):
-    def get(self):
-        #self.response.headers['Content-Type'] = 'text/plain'
-        q = self.request.get("q")
-        self.response.out.write(self.request)
-
+        userDay = self.request.get('day')
+        userMonth = self.request.get('month')
+        userYear = self.request.get('year')
+        if userDay.isdigit() and userMonth.isdigit() and userYear.isdigit():
+            self.write_form("That's okay.")
+        else:
+            self.write_form("Invalid date.")
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/testform', TestHandler)
+    ('/', MainPage)
 ], debug=True)
