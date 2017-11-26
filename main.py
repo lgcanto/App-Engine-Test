1a7f6c4850c318a0af46db13b9ce5238106bf2eb
lgcanto <lucascanto@poli.ufrj.br>
2017-11-26 19:32:33 -0200

    Changes due the "signup" assignment

--- main.py
+++ main.py
@@ -1,71 +1,79 @@
 import webapp2
 import cgi
 import string
+import re
 
-with open('forms.html', 'r') as myfile:
-    form=myfile.read()
+with open('welcome.html', 'r') as myfile:
+    welcome=myfile.read()
+
+with open('signup.html', 'r') as myfile:
+    signup=myfile.read()
 
 def escape_html(text):
     return cgi.escape(text, quote = True)
 
-rot13 = string.maketrans( 
-    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
-    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
-
-inverse_rot13 = string.maketrans( 
-    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm", 
-    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz")
-
-ToROT13 = True
-
-class ROT13(webapp2.RequestHandler):
-    
-    def write_form(self, text = ""):
-        self.response.out.write(form % {"user_text": text})
+class Signup(webapp2.RequestHandler):
+    def write_signup(self, username = "",
+                           email = "",
+                           username_error = "",
+                           password_error = "",
+                           verifypass_error = "",
+                           email_error = ""):
+        self.response.out.write(signup % {"user_username": username,
+                                          "user_email": email,
+                                          "username_error": username_error,
+                                          "passsword_error": password_error,
+                                          "verifypass_error": verifypass_error,
+                                          "email_error": email_error})
 
     def get(self):
-        self.write_form()
+        self.write_signup()
         
     def post(self):
-        global ToROT13
-        text = str(self.request.get('text'))
-        if ToROT13:
-            text = string.translate(text, rot13)
-            ToROT13  = False
-        else:
-            text = string.translate(text, inverse_rot13)
-            ToROT13  = True
+        usernameRegex = "^[a-zA-Z0-9_-]{3,20}$"
+        passwordRegex = "^.{3,20}$"
+        emailRegex = "^[\S]+@[\S]+.[\S]+$"
+
+        userError = passError = verifyError = emailError = ""
+
+        userName = escape_html(self.request.get('username'))
+        email = escape_html(self.request.get('email'))
+        password = escape_html(self.request.get('password'))
+        confirmPassword = escape_html(self.request.get('verify'))
         
-        self.write_form(escape_html(text))
+        emailMatch = re.match(emailRegex, email)
+        passwordMatch = re.match(passwordRegex, password)
+        usernameMatch = re.match(usernameRegex, userName)
+
+        if (usernameMatch == None):
+            userError = "Invalid Username."
+        if (passwordMatch == None):
+            passError = "Invalid password."
+        if password != confirmPassword:
+            verifyError = "Passwords didn't match."
+        if (emailMatch == None) & (email != ""):
+            emailError = "Invalid e-mail."
+        
+        if (userError == "") & (passError == "") & (verifyError == "") & (emailError == ""):
+            self.redirect("/welcome?username=" + userName)
+        else:
+            self.write_signup(userName, email, userError, passError, verifyError, emailError)
+
+class Welcome(webapp2.RequestHandler):
+    
+    def write_welcome(self):
+        self.response.out.write(welcome % {"user_username": self.request.get('username')})
+    
+    def get(self):
+        self.write_welcome()
+
+class Redirect(webapp2.RequestHandler):
+    
+    def get(self):
+        self.redirect("/signup")
 
 app = webapp2.WSGIApplication([
-    ('/', ROT13)
+    ('/', Redirect),
+    ('/signup', Signup),
+    ('/welcome', Welcome)
 ], debug=True)
-
-# class MainPage(webapp2.RequestHandler):
-#     def write_form(self, message = "", month = "", day = "", year = ""):
-#         self.response.out.write(form % {"message": message,
-#                                         "month": month,
-#                                         "day": day,
-#                                         "year": year})
-    
-#     def get(self):
-#         self.write_form()
-    
-#     def post(self):
-#         userMonth = escape_html(str(self.request.get('month')))
-#         userDay = escape_html(str(self.request.get('day')))
-#         userYear = escape_html(str(self.request.get('year')))
-#         if not (userDay.isdigit() and userMonth.isdigit() and userYear.isdigit()):
-#             self.write_form("Invalid date.", userMonth, userDay, userYear)
-#         else:
-#             self.redirect("/thanks")
-
-# class Thanks(webapp2.RequestHandler):
-#     def get(self):
-#         self.response.out.write("Thank you for your submit.")
-
-# app = webapp2.WSGIApplication([
-#     ('/', MainPage),
-#     ('/thanks', Thanks)
-# ], debug=True)
